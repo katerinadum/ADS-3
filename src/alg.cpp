6ee76895553527b47834f07edf838eddd2d531dc
katerinadum <katya05102002@gmail.com>
2021-05-13 20:51:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,45 @@
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте сюда нужный код
-  return std::string("");
+  TStack<char> stack;
+  std::string result;
+  for (int i = 0; i < inf.length(); i++) {
+    a = inf[i];
+    if (a >= '0' && a <= '9') {
+      result += a;
+      result += ' ';
+    } else if (a == '(') {
+      stack.push(a);
+    } else if (priority(a) > priority(stack.get()) || stack.isEmpty()) {
+      stack.push(a);
+    } else if (a == ')') {
+      while (!stack.isEmpty() && stack.get() != '(') {
+        result += stack.get();
+        result += ' ';
+        stack.pop();
+      }
+      if (stack.get() == '(') {
+        stack.pop();
+      }
+    } else {
+      while (!stack.isEmpty() && priority(stack.get()) >= priority(a)) {
+        result += stack.get();
+        result += ' ';
+        stack.pop();
+      }
+      stack.push(a);
+    }
+  }
+  while (!stack.isEmpty()) {
+    result += stack.get();
+    result += ' ';
+    stack.pop();
+  }
+  while (result[result.length() - 1] == ' ') {
+    result = result.substr(0, result.length()-1);
+  }
+
+  return result;
 }
 
 int eval(std::string pst) {
@@ -12,15 +49,15 @@ int eval(std::string pst) {
   return 0;
 }
 
-int priority(char a) {
+int priority(char symbol) {
     int p = 0;
-    if (a == '(')
-        p = 0;
-    if (a == ')')
-        p = 1;
-    if (a == '+' || a == '-')
-        p = 2;
-    if (a == '*' || a == '/')
-        p = 3;
-    return p;
+    if (symbol == '(')
+        return 0;
+    if (symbol == ')')
+        return 1;
+    if (symbol == '+' || a == '-')
+        return 2;
+    if (symbol == '*' || a == '/')
+        return 3;
+    return -1;
 }
